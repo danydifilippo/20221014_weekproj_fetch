8fd88779defdacd76cf6acc78998014a41008364
danydifilippo <danydifilippo@gmail.com>
2022-10-14 19:50:46 +0200

    video_cards

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
+
 async function showApi() {
     let musiclink = await fetch ('https://striveschool-api.herokuapp.com/api/deezer/search?q=maneskin');
     let responseText = await musiclink.json();
     console.log(responseText);
     let music = responseText.data;
     console.log(music);
- 
+    
     for (let i=0; i<music.length; i++){
         let showcard = document.getElementById('cards')
         let scheda = document.createElement('div');
         scheda.classList.add('card')
         scheda.innerHTML += `<img src="${music[i].album.cover}" class="card-img-top" alt="cover album Maneskin">
         <div class="card-body"><h5 class="card-title">${music[i].album.title}</h5>
-          <audio controls style="width: 500px;"><source src="${music[i].preview}" type="audio/ogg"></audio>
-          <a href="${music[i].artist.link}" class="btn btn-primary">Go to link</a>`
+        <audio controls style="width: 500px;"><source src="${music[i].preview}" type="audio/ogg"></audio>
+        <a href="${music[i].artist.link}" class="btn btn-primary">Go to link</a>`
         showcard.appendChild(scheda);
     }
+    
+    const songList = document.getElementById('songlist');
+    const searchBar = document.getElementById('searchBar');
+    console.log(searchBar);
+    searchBar.addEventListener('keyup',(input) =>{
+        const searchString = input.target.value;
+        const filteredSongs = music.filter( song =>{
+        return song.album.title.includes(searchString)
+    });
+    console.log(filteredSongs)
+    })
 
 }
 
@@ -30,6 +42,5 @@ function pausa()
 {
     sound.pause();
 }
-
 
 
