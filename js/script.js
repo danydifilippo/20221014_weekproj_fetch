64d0ff9b7c5a1cc7b2023596f5f5241c40f76fed
danydifilippo <danydifilippo@gmail.com>
2022-10-17 00:49:05 +0200

    final

--- js/script.js
+++ js/script.js
@@ -9,25 +9,36 @@ async function showApi() {
     for (let i=0; i<music.length; i++){
         let showcard = document.getElementById('cards')
         let scheda = document.createElement('div');
-        scheda.classList.add('card')
-        scheda.innerHTML += `<img src="${music[i].album.cover}" class="card-img-top" alt="cover album Maneskin">
-        <div class="card-body"><h5 class="card-title">${music[i].album.title}</h5>
-        <audio controls style="width: 500px;"><source src="${music[i].preview}" type="audio/ogg"></audio>
+        scheda.classList.add('card', 'bg-transparent', 'border-0')
+        scheda.innerHTML += `<img src="${music[i].album.cover_medium}" class="card-img-top rounded-circle" alt="cover album Maneskin">
+        <div class="card-body"><h5 class="card-title bg-white bg-opacity-25 rounded-pill ">${music[i].title}</h5>
+        <audio controls style="width: 180px;"><source src="${music[i].preview}" type="audio/ogg"></audio>
         <a href="${music[i].artist.link}" class="btn btn-primary">Go to link</a>`
         showcard.appendChild(scheda);
     }
     
-
-    const songList = document.getElementById('songlist');
+ 
     const searchBar = document.getElementById('searchBar');
+ 
     console.log(searchBar);
     searchBar.addEventListener('keyup',(e) =>{
-        e = searchBar.value;
+        const searchString = e.target.value;
         const filteredSongs = music.filter( song =>{
-        return song.album.title.includes(searchString)
+        return song.title.toLowerCase().includes(searchString.toLowerCase())
     });
     console.log(filteredSongs)
-    })
+    let showcard = document.getElementById('cards')
+    showcard.innerHTML=''
+    for (let i=0; i<filteredSongs.length; i++){
+        let scheda = document.createElement('div');
+        scheda.classList.add('card', 'bg-transparent', 'border-0')
+        scheda.innerHTML += `<img src="${filteredSongs[i].album.cover_medium}" class="card-img-top rounded-circle" alt="cover album Maneskin">
+        <div class="card-body"><h5 class="card-title bg-white bg-opacity-25 rounded-pill ">${filteredSongs[i].title}</h5>
+        <audio controls style="width: 180px;"><source src="${filteredSongs[i].preview}" type="audio/ogg"></audio>
+        <a href="${filteredSongs[i].artist.link}" class="btn btn-primary">Go to link</a>`
+        showcard.appendChild(scheda);
+    }
+})
 
 }
 
@@ -43,5 +54,27 @@ function pausa()
 {
     sound.pause();
 }
+
+
+    
+
+async function showVideo() {
+    let videolink = await fetch ('../bestMoments.json');
+    let responsText = await videolink.json();
+    console.log(responsText);
+    let videoplayer = responsText.cover;
+    console.log(videoplayer);
+
+
+    for (let a=0; a<videoplayer.length; a++){
+        let showframe = document.getElementById('video')
+        let videoframe = document.createElement('div');
+        videoframe.classList.add('col-12')
+        videoframe.innerHTML += `<iframe class="rounded" width="760" height="415" src=${videoplayer[a].video} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+        showframe.appendChild(videoframe);
+    }
+}
+
+showVideo()
 
 
